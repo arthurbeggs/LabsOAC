5cbb3530905d59591e44f287a5846be523926396
Arthur Matos <arthurm104@gmail.com>
2016-04-17 17:31:06 -0300

    Arquivo sort_pt3_O1.s modificado [testado e OK]
    - Todas as alterações necessárias foram feitas e o programa roda OK

--- Lab1/Lab1a/Pt2_GCC/sort_pt3_O1.s
+++ Lab1/Lab1a/Pt2_GCC/sort_pt3_O1.s
@@ -1,75 +1,95 @@
-	.file	1 "sort.c"
-	.section .mdebug.abi32
-	.previous
-	.nan	legacy
-	.gnu_attribute 4, 1
-	.section	.rodata.str1.4,"aMS",@progbits,1
+	.data
 	.align	2
 .LC0:
-	.ascii	"%d\011\000"
+	.asciiz	"\t"
 	.text
-	.align	2
+
+	.globl	main
+main:
+	addiu	$sp,$sp,-24
+	sw	$31,20($sp)
+	sw	$16,16($sp)
+	# lui	$16,%hi(v)
+	# addiu	$4,$16,%lo(v)
+	la $4, v
+	li	$5,10			# 0xa						# Delayd branching desativado
+	jal	show
+
+	# addiu	$4,$16,%lo(v)
+	la $4, v
+	li	$5,10			# 0xa						# Delayd branching desativado
+	jal	sort
+
+	# addiu	$4,$16,%lo(v)
+	la $4, v
+	li	$5,10			# 0xa						# Delayd branching desativado
+	jal	show
+
+	lw	$31,20($sp)
+	lw	$16,16($sp)
+	addiu	$sp,$sp,24
+	# j	$31
+	j exit											# Delayd branching desativado
+
+
+printf:
+	move $t0, $a0			# Salva endereço de "\t"
+	move $a0, $a1			# $a0 recebe v[iterador]
+	li $v0, 1				# print integer syscall
+	syscall
+	move $a0, $t0			# $a0 recebe "\t"
+	li $v0, 4				# print string syscall
+	syscall
+	jr $ra					# Retorna
+putchar:
+	li $v0, 11				# print char syscall
+	syscall
+	jr $ra					# Retorna
+exit:
+	li $v0, 10				# exit0 syscall
+	syscall
+
+
+
 	.globl	show
-	.set	nomips16
-	.set	nomicromips
-	.ent	show
-	.type	show, @function
 show:
-	.frame	$sp,40,$31		# vars= 0, regs= 5/0, args= 16, gp= 0
-	.mask	0x800f0000,-4
-	.fmask	0x00000000,0
-	.set	noreorder
-	.set	nomacro
 	addiu	$sp,$sp,-40
 	sw	$31,36($sp)
 	sw	$19,32($sp)
 	sw	$18,28($sp)
 	sw	$17,24($sp)
 	sw	$16,20($sp)
+	move	$18,$5									# Delayd branching desativado		<<<<
 	blez	$5,.L2
-	move	$18,$5
 
 	move	$17,$4
 	move	$16,$0
-	lui	$19,%hi(.LC0)
-	addiu	$19,$19,%lo(.LC0)
+	# lui	$19,%hi(.LC0)
+	# addiu	$19,$19,%lo(.LC0)
+	la $19, .LC0
 .L3:
 	move	$4,$19
+	lw	$5,0($17)									# Delayd branching desativado		<<<<
 	jal	printf
-	lw	$5,0($17)
 
 	addiu	$16,$16,1
+	addiu	$17,$17,4								# Delayd branching desativado		<<<<
 	bne	$16,$18,.L3
-	addiu	$17,$17,4
 
 .L2:
+	li	$4,10			# 0xa						# Delayd branching desativado
 	jal	putchar
-	li	$4,10			# 0xa
 
 	lw	$31,36($sp)
 	lw	$19,32($sp)
 	lw	$18,28($sp)
 	lw	$17,24($sp)
 	lw	$16,20($sp)
-	j	$31
-	addiu	$sp,$sp,40
+	addiu	$sp,$sp,40								# Delayd branching desativado
+	jr	$31
 
-	.set	macro
-	.set	reorder
-	.end	show
-	.size	show, .-show
-	.align	2
 	.globl	swap
-	.set	nomips16
-	.set	nomicromips
-	.ent	swap
-	.type	swap, @function
 swap:
-	.frame	$sp,0,$31		# vars= 0, regs= 0/0, args= 0, gp= 0
-	.mask	0x00000000,0
-	.fmask	0x00000000,0
-	.set	noreorder
-	.set	nomacro
 	sll	$5,$5,2
 	addu	$2,$4,$5
 	lw	$3,0($2)
@@ -77,25 +97,11 @@ swap:
 	addu	$4,$4,$5
 	lw	$5,0($4)
 	sw	$5,0($2)
-	j	$31
-	sw	$3,0($4)
+	sw	$3,0($4)									# Delayd branching desativado
+	jr	$31
 
-	.set	macro
-	.set	reorder
-	.end	swap
-	.size	swap, .-swap
-	.align	2
 	.globl	sort
-	.set	nomips16
-	.set	nomicromips
-	.ent	sort
-	.type	sort, @function
 sort:
-	.frame	$sp,48,$31		# vars= 0, regs= 8/0, args= 16, gp= 0
-	.mask	0x807f0000,-4
-	.fmask	0x00000000,0
-	.set	noreorder
-	.set	nomacro
 	blez	$5,.L14
 	nop
 
@@ -112,8 +118,8 @@ sort:
 	move	$20,$4
 	addiu	$22,$5,-1
 	move	$21,$0
+	li	$19,-1			# 0xffffffffffffffff		# Delayd branching desativado
 	j	.L8
-	li	$19,-1			# 0xffffffffffffffff
 
 .L11:
 	bltz	$16,.L9
@@ -122,13 +128,13 @@ sort:
 	lw	$3,0($20)
 	lw	$2,4($20)
 	slt	$2,$2,$3
+	move	$4,$18									# Delayd branching desativado
 	beq	$2,$0,.L9
-	move	$4,$18
 
 	move	$17,$20
 .L15:
+	move	$5,$16									# Delayd branching desativado
 	jal	swap
-	move	$5,$16
 
 	addiu	$16,$16,-1
 	beq	$16,$19,.L9
@@ -138,15 +144,15 @@ sort:
 	addiu	$17,$17,-4
 	lw	$2,4($17)
 	slt	$2,$2,$3
+	move	$4,$18									# Delayd branching desativado
 	bne	$2,$0,.L15
-	move	$4,$18
 
 .L9:
 	addiu	$21,$21,1
 	addiu	$20,$20,4
 .L8:
+	move	$16,$21									# Delayd branching desativado
 	bne	$21,$22,.L11
-	move	$16,$21
 
 	lw	$31,44($sp)
 	lw	$22,40($sp)
@@ -158,55 +164,14 @@ sort:
 	lw	$16,16($sp)
 	addiu	$sp,$sp,48
 .L14:
-	j	$31
+	jr	$31
 	nop
 
-	.set	macro
-	.set	reorder
-	.end	sort
-	.size	sort, .-sort
-	.align	2
-	.globl	main
-	.set	nomips16
-	.set	nomicromips
-	.ent	main
-	.type	main, @function
-main:
-	.frame	$sp,24,$31		# vars= 0, regs= 2/0, args= 16, gp= 0
-	.mask	0x80010000,-4
-	.fmask	0x00000000,0
-	.set	noreorder
-	.set	nomacro
-	addiu	$sp,$sp,-24
-	sw	$31,20($sp)
-	sw	$16,16($sp)
-	lui	$16,%hi(v)
-	addiu	$4,$16,%lo(v)
-	jal	show
-	li	$5,10			# 0xa
 
-	addiu	$4,$16,%lo(v)
-	jal	sort
-	li	$5,10			# 0xa
 
-	addiu	$4,$16,%lo(v)
-	jal	show
-	li	$5,10			# 0xa
-
-	lw	$31,20($sp)
-	lw	$16,16($sp)
-	j	$31
-	addiu	$sp,$sp,24
-
-	.set	macro
-	.set	reorder
-	.end	main
-	.size	main, .-main
 	.globl	v
 	.data
 	.align	2
-	.type	v, @object
-	.size	v, 40
 v:
 	.word	5
 	.word	8
@@ -218,4 +183,3 @@ v:
 	.word	0
 	.word	1
 	.word	9
-	.ident	"GCC: (Sourcery CodeBench Lite 2013.11-37) 4.8.1"
