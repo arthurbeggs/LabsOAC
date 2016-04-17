c71c86ca03ae0c8237c63fc1806ba913888663f4
Arthur Matos <arthurm104@gmail.com>
2016-04-17 19:22:21 -0300

    Arquivo sort_pt3_Os.s modificado [testado e OK]
    - Todas as alterações necessárias foram feitas e o programa roda OK
    - A instrução bltzl foi trocada
    - Obs.: É necessário ativar o delayed branching no MARS

--- Lab1/Lab1a/Pt2_GCC/sort_pt3_Os.s
+++ Lab1/Lab1a/Pt2_GCC/sort_pt3_Os.s
@@ -1,28 +1,62 @@
-	.file	1 "sort.c"
-	.section .mdebug.abi32
-	.previous
-	.nan	legacy
-	.gnu_attribute 4, 1
-	.section	.rodata.str1.4,"aMS",@progbits,1
+#  Delayed branching ativo no MARS
+	.data
 	.align	2
 .LC0:
-	.ascii	"%d\011\000"
+	.asciiz	"\t"
 	.text
-	.align	2
+
+	.globl	main
+main:
+	addiu	$sp,$sp,-24
+	sw	$16,16($sp)
+	# lui	$16,%hi(v)
+	# addiu	$4,$16,%lo(v)
+	la $4, v
+	sw	$31,20($sp)
+	jal	show
+	li	$5,10			# 0xa
+
+	# addiu	$4,$16,%lo(v)
+	la $4, v
+	jal	sort
+	li	$5,10			# 0xa
+
+	lw	$31,20($sp)
+	# addiu	$4,$16,%lo(v)
+	la $4, v
+	lw	$16,16($sp)
+	li	$5,10			# 0xa
+	jal	show
+	addiu	$sp,$sp,24
+
+	j exit
+	nop
+
+
+printf:
+	move $t0, $a0			# Salva endereço de "\t"
+	move $a0, $a1			# $a0 recebe v[iterador]
+	li $v0, 1				# print integer syscall
+	syscall
+	move $a0, $t0			# $a0 recebe "\t"
+	li $v0, 4				# print string syscall
+	syscall
+	jr $ra					# Retorna
+putchar:
+	li $v0, 11				# print char syscall
+	syscall
+	jr $ra					# Retorna
+exit:
+	li $v0, 10				# exit0 syscall
+	syscall
+
+
 	.globl	show
-	.set	nomips16
-	.set	nomicromips
-	.ent	show
-	.type	show, @function
 show:
-	.frame	$sp,40,$31		# vars= 0, regs= 5/0, args= 16, gp= 0
-	.mask	0x800f0000,-4
-	.fmask	0x00000000,0
-	.set	noreorder
-	.set	nomacro
 	addiu	$sp,$sp,-40
 	sw	$19,32($sp)
-	lui	$19,%hi(.LC0)
+	# lui	$19,%hi(.LC0)
+	la $19, .LC0
 	sw	$18,28($sp)
 	sw	$17,24($sp)
 	sw	$16,20($sp)
@@ -30,7 +64,7 @@ show:
 	move	$18,$4
 	move	$17,$5
 	move	$16,$0
-	addiu	$19,$19,%lo(.LC0)
+	# addiu	$19,$19,%lo(.LC0)
 	slt	$2,$16,$17
 .L7:
 	beq	$2,$0,.L6
@@ -55,22 +89,8 @@ show:
 	j	putchar
 	addiu	$sp,$sp,40
 
-	.set	macro
-	.set	reorder
-	.end	show
-	.size	show, .-show
-	.align	2
 	.globl	swap
-	.set	nomips16
-	.set	nomicromips
-	.ent	swap
-	.type	swap, @function
 swap:
-	.frame	$sp,0,$31		# vars= 0, regs= 0/0, args= 0, gp= 0
-	.mask	0x00000000,0
-	.fmask	0x00000000,0
-	.set	noreorder
-	.set	nomacro
 	sll	$5,$5,2
 	addu	$2,$4,$5
 	addiu	$5,$5,4
@@ -78,40 +98,30 @@ swap:
 	lw	$3,0($2)
 	lw	$5,0($4)
 	sw	$5,0($2)
-	j	$31
+	jr	$31
 	sw	$3,0($4)
 
-	.set	macro
-	.set	reorder
-	.end	swap
-	.size	swap, .-swap
-	.align	2
 	.globl	sort
-	.set	nomips16
-	.set	nomicromips
-	.ent	sort
-	.type	sort, @function
 sort:
-	.frame	$sp,24,$31		# vars= 0, regs= 1/0, args= 16, gp= 0
-	.mask	0x80000000,-4
-	.fmask	0x00000000,0
-	.set	noreorder
-	.set	nomacro
 	addiu	$sp,$sp,-24
-	move	$9,$4
-	move	$10,$5
-	sw	$31,20($sp)
-	move	$6,$0
+	move	$9,$4							# $t1 recebe $a0 (v)
+	move	$10,$5							# $t2 recebe $a1 (size(v))
+	sw	$31,20($sp)							# Salva $ra no stack
+	move	$6,$0							# $a2 recebe 0
 .L10:
-	slt	$2,$6,$10
-	beq	$2,$0,.L16
-	sll	$7,$6,2
+	slt	$2,$6,$10							# $v0 = $a2 < $t2 ? 1 : 0
+	beq	$2,$0,.L16							# jal .L16 se $v0 == 0
+	sll	$7,$6,2								# $a3 = $a2 * 4  [executado antes de beq]
 
-	addiu	$8,$6,-1
-	addu	$7,$9,$7
+	addiu	$8,$6,-1						# $t0 = $a2 - 1
+	addu	$7,$9,$7						# $a3 += $t1
 .L11:
-	bltzl	$8,.L10
+	# bltzl	$8,.L10							# jal .L10 se $t0 < 0				# Otimização de pipeline branch-likely não suportada
+	bltz	$8,.L10
 	addiu	$6,$6,1
+
+	addiu $6,$6,-1							# Desfaz a operação anterior caso o branching não aconteça
+
 
 	lw	$3,-4($7)
 	addiu	$7,$7,-4
@@ -132,54 +142,13 @@ sort:
 
 .L16:
 	lw	$31,20($sp)
-	j	$31
+	jr	$31
 	addiu	$sp,$sp,24
 
-	.set	macro
-	.set	reorder
-	.end	sort
-	.size	sort, .-sort
-	.section	.text.startup,"ax",@progbits
-	.align	2
-	.globl	main
-	.set	nomips16
-	.set	nomicromips
-	.ent	main
-	.type	main, @function
-main:
-	.frame	$sp,24,$31		# vars= 0, regs= 2/0, args= 16, gp= 0
-	.mask	0x80010000,-4
-	.fmask	0x00000000,0
-	.set	noreorder
-	.set	nomacro
-	addiu	$sp,$sp,-24
-	sw	$16,16($sp)
-	lui	$16,%hi(v)
-	addiu	$4,$16,%lo(v)
-	sw	$31,20($sp)
-	jal	show
-	li	$5,10			# 0xa
 
-	addiu	$4,$16,%lo(v)
-	jal	sort
-	li	$5,10			# 0xa
-
-	lw	$31,20($sp)
-	addiu	$4,$16,%lo(v)
-	lw	$16,16($sp)
-	li	$5,10			# 0xa
-	j	show
-	addiu	$sp,$sp,24
-
-	.set	macro
-	.set	reorder
-	.end	main
-	.size	main, .-main
 	.globl	v
 	.data
 	.align	2
-	.type	v, @object
-	.size	v, 40
 v:
 	.word	5
 	.word	8
@@ -191,4 +160,3 @@ v:
 	.word	0
 	.word	1
 	.word	9
-	.ident	"GCC: (Sourcery CodeBench Lite 2013.11-37) 4.8.1"
