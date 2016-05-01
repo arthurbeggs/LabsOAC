943ed0a529316dde019632a129a0647e4a36bd28
Arthur Matos <arthurm104@gmail.com>
2016-05-01 19:50:58 -0300

    Diversas mudanças:
    * Mudanças na pasta Lab1/Lab1a/Pt3_Primos:
    - twin_primes_Pt1.s e twin_primes_Pt2.s removidos;
    - twinPrimes.s agora responde aos itens 3.1 e 3.2 do roteiro;
    - O arquivo twinPrimes.s sofreu pequenas modificações e está 100% funcional.
    
    * Mudanças nas pastas Lab2/Lab2a/Pt1_FloatPoint e Pt2_FixedPoint:
    - Os arquivos show___.s foram removidos. Os procedimentos "show" agora ficarão no mesmo arquivo dos procedimentos "bhaskara".

--- Lab1/Lab1a/Pt3_Primos/twinPrimes.s
+++ Lab1/Lab1a/Pt3_Primos/twinPrimes.s
@@ -1,67 +1,91 @@
 ##
 #  Lab1 - Calculo de Primos Gemeos
-# /version      release v2
+# /version      release v3
 # /authors      Gabriel Iduarte | Rafael Lima | Arthur Matos
 
 ##
 # Macros
 ##
 .eqv MAX_DIV_POSSIVEL           65534       # Divisor máximo limitado por sqrt(2^32)
+.eqv INDEX_PROCURADO            $a0         # Índice do conjunto desejado de primos gêmeos
 .eqv NUM_EM_TESTE               $s0         # Numero em avaliação de primalidade
-.eqv INDEX_PROCURADO            $s1         # Índice do conjunto desejado de primos gêmeos
 .eqv ULTIMO_GEMEO_ENCONTRADO    $s2         # Guarda o valor do último gêmeo encontrado
 .eqv POSSIVEL_GEMEO             $s3         # Valor que possivelmente é gêmeo
 .eqv INDEX_ATUAL                $s4         # Índice do último conjunto de primos gêmeos encontrado
-.eqv LIMITE_NUM_EM_TESTE        $s5         # Número em teste limite para 32 bits
 .eqv MAIOR_DIV                  $s6         # Divisor máximo do número atualmente em teste de primalidade
 .eqv INDEX_LISTA                $s7         # Índice de posição da lista de primos menores que MAIOR_DIVISOR_POSSIVEL
+.eqv LIMITE_NUM_EM_TESTE        $s5         # Número em teste limite para 32 bits
 # Temporários não modificáveis: $t7, $t5
 
-.macro print_str (%string)                  # Printa string na tela
+.macro _recebe_int_                         # Lê do teclado o INDEX_PROCURADO
+    li      $v0, 5
+    syscall
+    move    INDEX_PROCURADO, $v0
+    ble     INDEX_PROCURADO, $zero, erro    # Se o índice digitado for zero, termina programa.
+.end_macro
+
+.macro _print_str_ (%string)                # Printa string na tela
     la      $a0, %string
     li      $v0, 4
     syscall
 .end_macro
 
-.macro print_int (%integer)                 # Printa inteiro na tela
+.macro _print_int_ (%integer)               # Printa inteiro na tela
     move    $a0, %integer
     li      $v0, 36
     syscall
 .end_macro
 
-.macro done                                 # Limpa o stack e faz a chamada de sistema exit(0)
+.macro _print_resultado_
+    move    $t0, $a0
+    move    $t1, $v0
+    _print_str_ (str_field1)                # printf ("Primos Gêmeos(")
+    _print_int_ ($t0)                       # printf ("%d",INDEX_ATUAL)
+    _print_str_ (str_field2)                # printf ("\)=\(")
+    _print_int_ ($t1)                       # printf ("%d",P[i-1])
+    addi    $t1, $t1, 2                     # Incrementa o último gêmeo encontrado em 2
+    _print_str_ (str_field3)                # printf (",")
+    _print_int_ ($t1)                       # printf ("%d",P[i])
+    _print_str_ (str_field4)                # printf ("\)\n")
+.end_macro
+
+.macro _exit_                               # Faz a chamada de sistema exit(0)
+    li      $v0, 10
+    syscall
+.end_macro
+
+.macro _empilha_registradores_
+    addi    $sp, $sp, -40
+    sw      $fp, 36($sp)
+    addi    $fp, $sp, 36
+    sw      $a0, 32($sp)
+    sw      $s0, 28($sp)
+    sw      $s2, 24($sp)
+    sw      $s3, 20($sp)
+    sw      $s4, 16($sp)
+    sw      $s5, 12($sp)
+    sw      $s6, 8($sp)
+    sw      $s7, 4($sp)
+    li      $t0, 3
+    sw      $t0, 0($sp)
+.end_macro
+
+.macro _desempilha_registradores_
     move    $sp, $fp
     lw      $fp, 0($sp)
-    lw      $s0, 4($sp)
-    lw      $s1, 8($sp)
-    lw      $s2, 12($sp)
-    lw      $s3, 16($sp)
-    lw      $s4, 20($sp)
-    lw      $s5, 24($sp)
-    lw      $s6, 28($sp)
-    lw      $s7, 32($sp)
+    lw      $a0, -4($sp)
+    lw      $s0, -8($sp)
+    lw      $s2, -12($sp)
+    lw      $s3, -16($sp)
+    lw      $s4, -20($sp)
+    lw      $s5, -24($sp)
+    lw      $s6, -28($sp)
+    lw      $s7, -32($sp)
     addiu   $sp, $sp, 4
-    jr $ra
 .end_macro
 
-.macro empilha_registradores                # Empilha registradores $sX e $fp
-addi    $sp, $sp, -40
-sw      $fp, 36($sp)
-addi    $fp, $sp, 36
-sw      $s0, 32($sp)
-sw      $s1, 28($sp)
-sw      $s2, 24($sp)
-sw      $s3, 20($sp)
-sw      $s4, 16($sp)
-sw      $s5, 12($sp)
-sw      $s6, 8($sp)
-sw      $s7, 4($sp)
-li      $t0, 3
-sw      $t0, 0($sp)
-.end_macro
-
-.macro inicia_registradores                 # Coloca o valor inicial nos registradores
-    empilha_registradores                   # Salva os registradores $sX
+.macro _inicia_registradores_               # Coloca o valor inicial nos registradores
+    _empilha_registradores_                 # Salva os registradores $sX
     li      NUM_EM_TESTE, 5                 # O numero a ser testado recebe 5
     li      POSSIVEL_GEMEO, 3               # POSSIVEL_GEMEO contém o candidato a gêmeo (5) que será testado.
     li      MAIOR_DIV, MAX_DIV_POSSIVEL     # MAIOR_DIV recebe MAX_DIV_POSSIVEL
@@ -73,10 +97,9 @@ sw      $t0, 0($sp)
     li      $t5, 2
     li      $t7, 6
     li      $t8, 2                          # Raiz de 5 inicializada
-    # li      $a0, 10000                      # Usado para testar o programa
 .end_macro
 
-.macro sqroot                               # Calcula sqrt de NUM_EM_TESTE e retorna parte inteira do resultado em $t8
+.macro _sqroot_                             # Calcula sqrt de NUM_EM_TESTE e retorna parte inteira do resultado em $t8
         mtc1    NUM_EM_TESTE, $f0           # Passa NUM_EM_TESTE para o Coprocessador 1
         cvt.d.w $f2, $f0                    # Converte NUM_EM_TESTE de word para double
         c.lt.d  $f2, $f18                   # Se $f2 < 0, seta flag bc1 do Coprocessador como true
@@ -88,12 +111,12 @@ sw      $t0, 0($sp)
         mfc1    $t8, $f2                    # $t8 recebe o resultado convertido
 .end_macro
 
-.macro incrementa_num_em_teste              # Incrementa NUM_EM_TESTE pulando múltiplos de 3 e testando overflow
+.macro _incrementa_num_em_teste_            # Incrementa NUM_EM_TESTE pulando múltiplos de 3 e testando overflow
         addu    NUM_EM_TESTE, NUM_EM_TESTE, $t5         # Incrementa NUM_EM_TESTE
         beq     NUM_EM_TESTE, LIMITE_NUM_EM_TESTE, erro # Testa se NUM_EM_TESTE chegou em 0xfffffffc
-        addi    $t5, $t5, 2                 # Incrementa $t5 em 2
-        bne     $t5, $t7, continua          # Se $t5 != 6, pula próxima instrução
-        li      $t5, 2                      # Seta $t5 como 2
+        addi    $t5, $t5, 2                     # Incrementa $t5 em 2
+        bne     $t5, $t7, continua              # Se $t5 != 6, pula próxima instrução
+        li      $t5, 2                          # Seta $t5 como 2
     continua:
 .end_macro
 
@@ -101,27 +124,37 @@ sw      $t0, 0($sp)
 # Data Segment
 ##
 .data
-    str_erro:       .asciiz "Maior primo representavel em 32 bits alcancado.\n"
-    str_primos:     .asciiz "Primos Gemeos:\n"
+    str_input:      .asciiz "Digite o valor i: "
+    str_erro:       .asciiz "Nao e possivel encontrar o i-esimo par desejado.\n"
+    str_imax:       .asciiz "O imax encontrado foi:\n"
+    str_field1:     .asciiz "Primos Gemeos("
+    str_field2:     .asciiz ")=("
+    str_field3:     .asciiz ","
+    str_field4:     .asciiz ")\n"
     str_newl:       .asciiz "\n"
     str_tab:        .asciiz "\t"
     d_zero:         .double 0.0
     d_full:         .double 4294967296.0
 
-    ##
-    # Text Segment
-    ##
+##
+# Text Segment
+##
 .text
-pg:
-    inicia_registradores                    # Macro: Inicialização de registradores
-    move    INDEX_PROCURADO, $a0            # INDEX_PROCURADO é o argumento recebido em $a0
-    addi    INDEX_LISTA, $fp, -32           # Define início da lista
+main:
+    _print_str_ (str_input)                 # Macro: Pede input do usuário
+    _recebe_int_                            # Macro: Salva o inputo do usuário em INDEX_PROCURADO
+    jal     PG                              # Encontra primeiro primo gêmeo do índice pedido
+    _print_resultado_                       # Exibe resultado na tela
+    _exit_                                  # Finaliza o programa
 
+PG:                                         # Recebe o índice procurado no registrador $a0 e retorna o primeiro primo desse índice em $v0
+    _inicia_registradores_                  # Macro: Inicialização de registradores
+    addi    INDEX_LISTA, $fp, -32           # Define início da lista
     j exec                                  # Inicia a execução
 
 incrementaTeste:
-    incrementa_num_em_teste                 # Incrementa NUM_EM_TESTE pulando múltiplos de 3 e testando overflow
-    sqroot                                  # Macro: Encontra raiz quadrada de NUM_EM_TESTE
+    _incrementa_num_em_teste_               # Incrementa NUM_EM_TESTE pulando múltiplos de 3 e testando overflow
+    _sqroot_                                # Macro: Encontra raiz quadrada de NUM_EM_TESTE
     addi    INDEX_LISTA, $fp, -32           # Retorna o índice da lista de primos para a posição -1 (será incrementado na próxima instrução)
 
 exec:
@@ -148,13 +181,11 @@ gemeoEncontrado:
     addi    INDEX_ATUAL, INDEX_ATUAL, 1     # Incrementa o índice de busca atual
     bne     INDEX_PROCURADO, INDEX_ATUAL, incrementaTeste   # Se os índices atual e esperado não forem iguais, continua a iteração
 
-exibeResultado:
+retornaIndice:
     addi    $v0, ULTIMO_GEMEO_ENCONTRADO, -2# Decrementa em 2 o último gêmeo encontrado
-
-    j fim
+    _desempilha_registradores_              # Deleta o frame atual
+    jr      $ra
 
 erro:
-    print_str(str_erro)                     # Exibe mensagem de erro
-
-fim:
-    done                                    # Finaliza o programa
+    _print_str_ (str_erro)                  # Exibe mensagem de erro
+    _exit_
