e2f6807ab33eca5c2c06d4d35d33e5b8353d73e7
tutubarao <andre.almeida3@gmail.com>
2016-05-02 19:29:01 -0300

    Update add4v.v

--- Lab1/Lab1b/somador_portas/add4v.v
+++ Lab1/Lab1b/somador_portas/add4v.v
@@ -1,5 +1,5 @@
 /*
-add 8 bits
+add 4 bits
  */
  
 module add4v (
@@ -10,17 +10,13 @@ output oCARRY);
 
 
 
-wire c0, c1, c2; //, c3, c4, c5 ,c6;
+wire c0, c1, c2;
 
 //somadores unarios em série
 	//full_addv (input1, input2, carryIN, oSUM, carryOUT);
 full_addv x1 (iA[0], iB[0], gnd, oSUM[0], c0);
 full_addv x2 (iA[1], iB[1], c0, oSUM[1], c1);
 full_addv x3 (iA[2], iB[2], c1, oSUM[2], c2);
-full_addv x4 (iA[3], iB[3], c2, oSUM[3], oCARRY); //full_addv x4 (iA[3], iB[3], c2, oSUM[3], c3);
-//full_addv x5 (iA[4], iB[4], c3, oSUM[4], c4);
-//full_addv x6 (iA[5], iB[5], c4, oSUM[5], c5);
-//full_addv x7 (iA[6], iB[6], c5, oSUM[6], c6);
-//full_addv x8 (iA[7], iB[7], c6, oSUM[7], oCARRY);
+full_addv x4 (iA[3], iB[3], c2, oSUM[3], oCARRY); 
 
 endmodule
