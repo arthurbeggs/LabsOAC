afb5504602eb86983ada791f5dbea681774da6c8
Arthur Matos <arthurm104@gmail.com>
2016-06-08 21:57:33 -0300

    Mudanças para síntese correta do patch e mais algumas reindentações
    Patch de mudanças no processador v4.3 aplicado
    
    O processador MIPS-PUM-v4.3 agora possui as alterações realizadas na v4.2

--- Processors/MIPS-PUM-v4.3/Core/CPU/ALU.v
+++ Processors/MIPS-PUM-v4.3/Core/CPU/ALU.v
@@ -22,103 +22,101 @@ assign oZero = (oALUresult == 32'b0);
 
 initial
 begin
-	{HI,LO} <= 64'b0;
+    {HI,LO} <= 64'b0;
 end
 
-assign oOverflow = iControlSignal==OPADD ? 
-		((iA[31] == 0 && iB[31] == 0 &&  oALUresult[31] == 1) || (iA[31] == 1 && iB[31] == 1 && oALUresult[31] == 0))
-		: iControlSignal==OPSUB ? 
-			((iA[31] == 0 && iB[31] == 1 && oALUresult[31]== 1)|| (iA[31] == 1 && iB[31] == 0 && oALUresult[31] == 0))
-			: 1'b0;
+assign oOverflow = iControlSignal==OPADD ?
+        ((iA[31] == 0 && iB[31] == 0 &&  oALUresult[31] == 1) || (iA[31] == 1 && iB[31] == 1 && oALUresult[31] == 0))
+        : iControlSignal==OPSUB ?
+            ((iA[31] == 0 && iB[31] == 1 && oALUresult[31]== 1)|| (iA[31] == 1 && iB[31] == 0 && oALUresult[31] == 0))
+            : 1'b0;
 
 always @(*)
 begin
-	case (iControlSignal)
-		OPAND:
-			oALUresult	<= iA & iB;
-		OPOR:
-			oALUresult	<= iA | iB;
-		OPADD:
-			oALUresult	<= iA + iB;
-		OPMFHI:
-			oALUresult	<= HI;
-		OPSLL:
-			oALUresult	<= iB << iShamt;
-		OPMFLO:
-			oALUresult	<= LO;
-		OPSUB:
-			oALUresult	= iA - iB;
-		OPSLT:
-			oALUresult	<= iA < iB;
-		OPSRL:
-			oALUresult	<= iB >> iShamt;
-		OPSRA:
-			oALUresult	<= iB >>> iShamt;
-		OPXOR:
-			oALUresult	<= iA ^ iB;
-		OPSLTU:
-			oALUresult	<= $unsigned(iA) < $unsigned(iB);
-		OPNOR:
-			oALUresult	<= ~(iA | iB);
-		OPLUI:
-			oALUresult	<= {iB[15:0],16'b0};
-		OPSLLV:
-			oALUresult <= iB << iA[4:0];
-		OPSRAV:
-			oALUresult	<= iB >>> iA[4:0];
-		OPSRLV:
-			oALUresult <= iB >> iA[4:0];
-			
+    case (iControlSignal)
+        OPAND:
+            oALUresult  = iA & iB;
+        OPOR:
+            oALUresult  = iA | iB;
+        OPADD:
+            oALUresult  = iA + iB;
+        OPMFHI:
+            oALUresult  = HI;
+        OPSLL:
+            oALUresult  = iB << iShamt;
+        OPMFLO:
+            oALUresult  = LO;
+        OPSUB:
+            oALUresult  = iA - iB;
+        OPSLT:
+            oALUresult  = iA < iB;
+        OPSGT:                          //2016/1 - implementada para as operacoes bgtz e blez
+			oALUresult  = iA > iB;
+        OPSRL:
+            oALUresult  = iB >> iShamt;
+        OPSRA:
+            oALUresult  = iB >>> iShamt;
+        OPXOR:
+            oALUresult  = iA ^ iB;
+        OPSLTU:
+            oALUresult  = $unsigned(iA) < $unsigned(iB);
+        OPNOR:
+            oALUresult  = ~(iA | iB);
+        OPLUI:
+            oALUresult  = {iB[15:0],16'b0};
+        OPSLLV:
+            oALUresult  = iB << iA[4:0];
+        OPSRAV:
+            oALUresult  = iB >>> iA[4:0];
+        OPSRLV:
+            oALUresult  = iB >> iA[4:0];
+
 // para testes e simulacao
-		OPMULT:
-			oALUresult	<= LO;
-		OPDIV:
-			oALUresult	<= LO;
+        OPMULT:
+            oALUresult  = LO;
+        OPDIV:
+            oALUresult  = LO;
 //
 
-		default:
-			oALUresult	<= 32'b0;
-	endcase
+        default:
+            oALUresult  = 32'b0;
+    endcase
 end
 
 always @(posedge iCLK)
 begin
-	if (iRST)
-	begin
-		{HI,LO}	<= 64'b0;
-	end
-	else
-		case (iControlSignal)
-			OPMULT:
-				{HI,LO} <= iA * iB;
+    if (iRST)
+    begin
+        {HI,LO}    <= 64'b0;
+    end
+    else
+        case (iControlSignal)
+            OPMULT:
+                {HI,LO} <= iA * iB;
 
-			OPDIV:
-				begin
-					LO	<= iA / iB;
-					HI	<= iA % iB;
-				end
-			
-			OPMULTU:
-				{HI,LO} <= $unsigned(iA) * $unsigned(iB);
+            OPDIV:
+                begin
+                    LO    <= iA / iB;
+                    HI    <= iA % iB;
+                end
 
-			OPDIVU:
-				begin
-					LO	<= $unsigned(iA) / $unsigned(iB);
-					HI	<= $unsigned(iA) % $unsigned(iB);
-				end
-			
-			// 2015/1
-			OPMTHI:
-				HI <= iA;
-			
-			// 2015/1
-			OPMTLO:
-				LO <= iA;
-		endcase
+            OPMULTU:
+                {HI,LO}   <= $unsigned(iA) * $unsigned(iB);
+
+            OPDIVU:
+                begin
+                    LO    <= $unsigned(iA) / $unsigned(iB);
+                    HI    <= $unsigned(iA) % $unsigned(iB);
+                end
+
+            // 2015/1
+            OPMTHI:
+                HI <= iA;
+
+            // 2015/1
+            OPMTLO:
+                LO <= iA;
+        endcase
 end
 
 endmodule
-
-
-
-
--- Processors/MIPS-PUM-v4.3/Core/CartaoSD/spi_interface.v
+++ Processors/MIPS-PUM-v4.3/Core/CartaoSD/spi_interface.v
@@ -1,62 +1,75 @@
-module my_spi_interface(readdata,writedata,write,clock,reset_n,SD_CLK,SD_MOSI,SD_MISO,SD_CS);
-output [7:0] readdata;
-input [7:0] writedata;
-input write;
-input clock;
-input reset_n;
-output SD_CLK;
-output SD_MOSI;
-input SD_MISO;
-output SD_CS;
-
-
-reg [3:0] ff;
-wire enable;
-/*
-assign enable = (chip_select && write);
-assign readdata[0] = ff[0];
-assign readdata[1] = ff[1];
-assign readdata[2] = ff[2];
-assign readdata[3] = ff[3];
-assign readdata[31:4] = 28'b0;
-
-assign SD_CLK = ff[0];
-assign SD_MOSI = ff[1];
-assign SD_CS = ff[3];
-always@(posedge clock)
-begin
-	if(enable)
-	begin
-		if(reset_n)
-		begin
-			ff[0] <= writedata[0];
-			ff[1] <= writedata[1];
-			ff[2] <= SD_MISO;
-			ff[3] <= writedata[3];
-		end
-		else
-		begin
-			ff[0] <= 1'b0;
-			ff[1] <= 1'b0;
-			ff[2] <= 1'b0;
-			ff[3] <= 1'b0;
-		end 
-	end
-end
-*/
- sd_controller sd1(
-	.cs(SD_CS),
-	.mosi(SD_MOSI),
-	.miso(SD_MISO),
-	.sclk(SD_CLK),
-
-	.rd(~write),
-	.wr(write),
-	.dm_in(1),	// data mode, 0 = write continuously, 1 = write single block
-	.reset(~reset_n),
-	.din(writedata[7:0]),
-	.dout(readdata[7:0]),
-	.clk(clock)
+module SPI_Interface(
+    input         iCLK,
+    input         iCLK_50,
+    input         Reset,
+    // input  [7:0]  readData,
+    // output [7:0]  writeData,
+    // input         write,
+    output        SD_CLK,
+    output        SD_MOSI,
+    input         SD_MISO,
+    output        SD_CS,
+    // Barramento de dados
+    input         wReadEnable, wWriteEnable,
+    input  [3:0]  wByteEnable,
+    input  [31:0] wAddress, wWriteData,
+    output [31:0] wReadData
 );
+
+
+sd_controller sd1(
+    .cs(SD_CS),
+    .mosi(SD_MOSI),
+    .miso(SD_MISO),
+    .sclk(SD_CLK),
+
+    .rd(1),
+    // .rd(SDReadEnable),
+    .wr(0),
+    .dm_in(1),                  // data mode, 0 = write continuously, 1 = write single block
+    .reset(Reset),
+    .din(8'b0),
+    .dout(SDData),
+    .address(SDAddress),
+    .iCLK(iCLK),
+    // .iCLK(iCLK_50),
+    .idleSD(SDCtrl)
+);
+
+
+reg  [31:0] SDAddress;
+wire [7:1]  SDData, SDCtrl;     // [SDCtrl ? BUSY : IDLE]
+wire        SDReadEnable;
+
+always @ (posedge iCLK)
+    if (wWriteEnable)
+    begin
+        if (wAddress == SD_INTERFACE_ADDR)
+        begin
+            SDAddress      <= wWriteData;
+            if (SDCtrl == 8'b0)
+                SDReadEnable    <= 1'b1;
+            else
+                SDReadEnable    <= 1'b0;
+        end
+        else    SDReadEnable    <= 1'b0;
+    end
+
+always @ (*)
+    if (wReadEnable)
+    begin
+        if (wAddress == SD_INTERFACE_DATA   ||  wAddress == SD_INTERFACE_CTRL)
+            wReadData   = {8'b0, 8'h42, SDData, SDCtrl};
+        else
+            wReadData   = 32'bz;
+    end
+    else    wReadData   = 32'bz;
+
+
+
+//TODO: Criar divisor de clock para que a frequência seja correta para cada etapa [init: 400 KHz] [pos-init: 25 MHz]    REVIEW: Implementado, mas não testado nem integrado
+
+//FIXME: Consertar ativação do SDReadEnable.
+
 
 endmodule
