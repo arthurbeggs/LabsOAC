2802bebc3d385df7d90d0f3eee6b7f43c07648f7
Arthur Matos <arthurm104@gmail.com>
2016-06-10 14:41:14 -0300

    Divisor de clock funcionando (aparentemente sempre). Nada no MISO e MOSI

--- Processors/MIPS-PUM-v4.2/Core/CartaoSD/clk_div.v
+++ Processors/MIPS-PUM-v4.2/Core/CartaoSD/clk_div.v
@@ -7,14 +7,14 @@ module CLK_Divider(
 
 reg         CLKfast, CLKslow;
 reg         COUNTERfast;
-reg  [5:0]  COUNTERslow;
+reg  [4:0]  COUNTERslow;
 
 initial
 begin
     CLKfast     <= 1'b0;
     CLKslow     <= 1'b0;
     COUNTERfast <= 1'b0;
-    COUNTERslow <= 6'b0;
+    COUNTERslow <= 5'b0;
 end
 
 always @(posedge CLKin)
@@ -22,49 +22,49 @@ begin
     CLKout  <= (State != 8'h00 && State < 8'h70) ? CLKslow : CLKfast;
 end
 
-always @(posedge CLKin)
-begin
-    if (COUNTERslow == 6'b1)            //NOTE: 781KHz
-        begin
-            CLKslow     <= ~CLKslow;
-            COUNTERslow <= 6'b0;
-        end
-    else    COUNTERslow <= COUNTERslow + 1'b1;
-
-    if (COUNTERfast == 1'b1)            //NOTE: 25MHz
-        begin
-            CLKfast     <= ~CLKfast;
-            COUNTERfast <= 1'b0;
-        end
-    else    COUNTERfast <= COUNTERfast + 1'b1;
-end
-
-// //NOTE: Bloco com reset
 // always @(posedge CLKin)
 // begin
-//     if (Reset)
-//     begin
-//         CLKfast     <= 1'b0;
-//         CLKslow     <= 1'b0;
-//         COUNTERfast <= 1'b0;
-//         COUNTERslow <= 6'b0;
-//     end
-//     else
-//     begin
-//         if (COUNTERslow == 6'b1)            //NOTE: 781KHz
-//             begin
-//                 CLKslow     <= ~CLKslow;
-//                 COUNTERslow <= 6'b0;
-//             end
-//         else    COUNTERslow <= COUNTERslow + 1'b1;
+//     if (COUNTERslow == 5'b11111)        //NOTE: 781KHz
+//         begin
+//             CLKslow     <= ~CLKslow;
+//             COUNTERslow <= 5'b0;
+//         end
+//     else    COUNTERslow <= COUNTERslow + 1'b1;
 //
-//         if (COUNTERfast == 1'b1)            //NOTE: 25MHz
-//             begin
-//                 CLKfast     <= ~CLKfast;
-//                 COUNTERfast <= 1'b0;
-//             end
-//         else    COUNTERfast <= COUNTERfast + 1'b1;
-//     end
+//     if (COUNTERfast == 1'b1)            //NOTE: 25MHz
+//         begin
+//             CLKfast     <= ~CLKfast;
+//             COUNTERfast <= 1'b0;
+//         end
+//     else    COUNTERfast <= COUNTERfast + 1'b1;
 // end
+
+//NOTE: Bloco com reset
+always @(posedge CLKin)
+begin
+    if (Reset)
+    begin
+        CLKfast     <= 1'b0;
+        CLKslow     <= 1'b0;
+        COUNTERfast <= 1'b0;
+        COUNTERslow <= 5'b0;
+    end
+    else
+    begin
+        if (COUNTERslow == 5'b11111)        //NOTE: 781KHz
+            begin
+                CLKslow     <= ~CLKslow;
+                COUNTERslow <= 5'b0;
+            end
+        else    COUNTERslow <= COUNTERslow + 1'b1;
+
+        if (COUNTERfast == 1'b1)            //NOTE: 25MHz
+            begin
+                CLKfast     <= ~CLKfast;
+                COUNTERfast <= 1'b0;
+            end
+        else    COUNTERfast <= COUNTERfast + 1'b1;
+    end
+end
 
 endmodule
