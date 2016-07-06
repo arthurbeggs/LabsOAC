1e609ae01215c4d7397a5ce4bbd842b1450ccc92
akafael <akafael@users.noreply.github.com>
2016-07-06 10:25:33 -0300

    Create ConversorWord_BitStream.py

---
+++ Utilities/ConversorWord_BitStream.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+
+import sys, getopt
+
+##
+# Conversor from asm mips words to binary big endian
+#
+# @author Rafael
+# @version 0.01
+##
+
+def main(argv):
+    inputfile = ''
+    outputfile = ''
+    try:
+       opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+    except getopt.GetoptError:
+       print 'test.py -i <inputfile> -o <outputfile>'
+       sys.exit(2)
+    for opt, arg in opts:
+       if opt == '-h':
+          print 'test.py -i <inputfile> -o <outputfile>'
+          sys.exit()
+       elif opt in ("-i", "--ifile"):
+          inputfile = arg
+       elif opt in ("-o", "--ofile"):
+          outputfile = arg
+    print 'Processing file "', inputfile,'"'
+
+    # Read File
+    mFile = open(inputfile,'r')        # Open File
+    fileData = '\n'+mFile.read()             # Read File
+
+    #fileData = \n+".word 0x01020304\n.word 0x11121314"
+
+    # Break into substrings
+    vectorLines = fileData.split("\n.word 0x")
+    newVectorLine = vectorLines
+
+    # take each substring and invert number
+    # and Glue Everithing together
+    for i in range(len(vectorLines)):
+        line = vectorLines[i]            # Get each substring
+        newVectorLine[i] = ''
+        if(line.find(':')==-1):            # Check if is label
+            # Change to bigendian
+            newline = str(chr(int(line[6:8],16))) + str(chr(int(line[4:6],16)))+str(chr(int(line[2:4],16)))+str(chr(int(line[0:2],16)))
+            newVectorLine[i] = newline
+
+    # Make a new file
+    mNewFile = open(outputfile,'w')
+    mNewFile.write(''.join(newVectorLine))
+    mNewFile.close()
+    mFile.close()
+    print 'Output file is "', outputfile,'"'
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
